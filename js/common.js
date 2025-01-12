5ffb7f75f9d468ed66ffa920dbc0b5c39e359158
Kristina Yancer <kristyjanzer.work@gmail.com>
2025-01-13 01:21:06 +0300

    end

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
-$(function() {
-
+$(document).ready(function() {
+  setTimeout(function(){
+    $('.overlay').fadeIn(400,
+      function() {
+          $('.comeback')
+          .css('display', 'block')
+          .animate({opacity: 1, top: '50%'}, 200);
+      });
+  }, 2000);
+  $('.comeback__close, .overlay').click(function () {
+    $('.comeback')
+      .animate({opacity: 0, top: '45%'}, 200,
+      function() {
+          $(this).css('display', 'none');
+          $('.overlay').fadeOut(400);
+      }
+      );
+  });
 });
